0e733c71883750d45a4f7ca642f2218a615aed37
Siarhei Kandratsiuk <siarhei_kandratsiuk@epam.com>
2022-06-02 00:33:31 +0300

    remove file

--- src/AreaUtil.js
+++
@@ -1,8 +0,0 @@
-const Aperture = require('./Aperture')
-const { summing } = require('./CalculationUtil');
-
-// module.exports = class AreaUtil {
-//     static getArea(zone) {
-//         return zone.height * zone.width - summing(zone.apertures, new Aperture(zone.height, zone.width).getApertureArea);
-//     }
-// }
